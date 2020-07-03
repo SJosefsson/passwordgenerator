020458f707687c2b8662d3d24413ae88eb965e97
SJosefsson <8761701+SJosefsson@users.noreply.github.com>
2020-07-03 09:38:30 +0200

    Updated generation map + removed hardcoded bit
    I realized that i forgot numbers in the map where the program generates the password from.
    Added 0-9 to the map
    Removed hardcoded endpoint in the random indexgenerator to fit if user modifies the map lenght.

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -3,10 +3,13 @@ import string, random
 map = list(string.ascii_letters)
 map.append('-')
 map.append('_')
+map.extend(range(10))
+
+print(map)
 
 passlength = int(input('How many characters do you want in your password? '))
 password = ''
 
-while len(password) < passlength: password += map[random.randint(0, 53)]
+while len(password) < passlength: password += str(map[random.randint(0, len(map) - 1)])
 
 print(password)
