3bf120f00428a4194cbbca815efe36c8a07c8d3e
Sean Jain Ellis <sellis@bandarji.com>
2020-07-18 14:18:37 -0700

    EOL characters

--- passwordgenerator/passwordgenerator.py
+++ passwordgenerator/passwordgenerator.py
@@ -9,9 +9,9 @@ def generate_password(valid_chars: str=VALID_CHARS, size: int=20) -> str:
     """Generates random password.
 
     Accepts:
-        valid_chars (str): Characters acceptable for password construction.
-        size (int): Count of characters for assembled password.
+        valid_chars (str) [VALID_CHARS]: Acceptable characters for password.
+        size (int) [20]: Count of characters for password.
     Returns:
-        str: Created password of length size from characters valid_chars.
+        str: Created password.
     """
     return ''.join(random.choice(valid_chars) for _ in range(size))
