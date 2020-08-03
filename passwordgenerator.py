a952009629b71b6709b6291481cae5085ab4c12d
Simon Josefsson <8761701+SJosefsson@users.noreply.github.com>
2020-08-03 21:25:53 +0200

    New updates
    * Corrected some code styling errors: Import extended to one line per import. While loop changed to for loop. Line length cut down according to PEP8. Calling string.digits instead of range(10).
    * Added special characters to be used when generating password.
    * Changed variable name 'map' to 'chars'
    * Changed random function to use secrets instead of random
    * Added a main function, also split user input and password generation to one function each, in order to fast call the password creation part directly from command line if wanted
    * Added Try and Except to verify user input and command line input.
    * Used .join to create the password variable, instead of +=-ing one character at a time to password variable

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,13 +1,39 @@
-import string, random
+import string
+import secrets
+import sys
 
-map = list(string.ascii_letters)
-map.append('-')
-map.append('_')
-map.extend(range(10))
+def main():
+    # Creating a list of all the characters that can be used when
+    # generating the password
+    chars = list(string.ascii_letters + string.digits
+    + '-' + '_' + '!' + '?' + '#' + '+' + '=' + '/' + '*' + '^'
+    + '£' + '$' + '%' + '&' + '(' + '[' + '{' + ')' + ']' + '}')
+    if len(sys.argv) > 1:
+        try:
+            password_creation(chars, int(sys.argv[1]))
+        except ValueError:
+            sys.exit('You need to enter a digit as argument!')
+    else:
+        passlength = user_input()
+        password_creation(chars, passlength)
 
-passlength = int(input('How many characters do you want in your password? '))
-password = ''
+def user_input():
+    # Asking user how long they want their password to be
+    while True:
+        try:
+            passlength = int(input(
+                'How many characters do you want in your password? '))
+            break
+        except ValueError:
+            print('Please input an integer')
+    return passlength
 
-while len(password) < passlength: password += str(map[random.randint(0, len(map) - 1)])
+def password_creation(chars, passlength):
+    # Creating the password
+    password = ''.join(secrets.choice(chars) for _ in range(passlength))
 
-print(password)
+    # Printing password so it can be copied
+    print(password)
+
+if __name__ == '__main__':
+    main()
