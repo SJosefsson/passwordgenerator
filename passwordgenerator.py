941ae569a68b1db9706d7ab09152dc9bb21f2d1f
ghanteyyy <ghanteyyy@gmail.com>
2020-08-17 17:29:28 +0545

    Multiple Fixes
    Since, you did-not like an idea of OOP. So, I fixed the following in your own style:
    
    - Fixed PEP E302 and E305.
    - Merged `main` and `password_creation` functions.
    - Re-arranged import as per their length (personal preferences :-) )
    - Removed **break** keyword as you can exit out of while loop with **return** keyword.
    - You do not have to make a list of `string.ascii_letters, string.digits, string.puncutation` because string itself is iterable. I have sliced all the values
      you needed using `string.printable[:94]`
    - Comments are written to explain some part of the code what they are doing or to inform why some functions or class were designed. In your code, I found that you have some unuseful comments so I removed them and added my own comments where I thought it was necessary.

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,42 +1,38 @@
+import sys
 import string
 import secrets
-import sys
 
-def main():
-    # Creating a list of all the characters that can be used when
-    # generating the password
-    chars = list(string.ascii_letters + string.digits + string.punctuation)
-
-    # Call password_creation with chars and user_input as arguments
-    password_creation(chars, user_input())
 
 def user_input():
+    '''Ask user to enter the length of password they want'''
+
     # If user called the program from command shell with an argument
-    # Check if argument is valid (int), and return it
-    # If argument is not valid exit program with an error.
     if len(sys.argv) > 1:
         try:
             return int(sys.argv[1])
+
         except ValueError:
             sys.exit('Error: You need to pass an integer as argument!')
 
     # If user did not call the program from command shell with an argument
-    # Ask user how long they want their password to be
     while True:
         try:
-            passlength = int(input(
-                'How many characters do you want in your password? '))
-            break
+            passlength = int(input('How many characters do you want in your password? '))
+            return passlength
+
         except ValueError:
-            print('Please input an integer')
-    return passlength
+            print('\nPlease input an integer\n')
 
-def password_creation(chars, passlength):
-    # Creating the password
+
+def main():
+    '''Generating password as per given length by user'''
+
+    chars = string.printable[:94]
+    passlength = user_input()
+
     password = ''.join(secrets.choice(chars) for _ in range(passlength))
-
-    # Printing password so it can be copied
     print(password)
+
 
 if __name__ == '__main__':
     main()
