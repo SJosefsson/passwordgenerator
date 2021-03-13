be46d8bb4e0d5629778ed48930464c2a64de6af0
insomniac-tk <shirleyhouse20@gmail.com>
2021-03-13 13:56:48 +0530

    Changed variable name of map to symbols.

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,13 +1,13 @@
 import string, random
 
-map = list(string.ascii_letters)
-map.append('-')
-map.append('_')
-map.extend(range(10))
+symbols = list(string.ascii_letters)
+symbols.append('-')
+symbols.append('_')
+symbols.extend(range(10))
 
 passlength = int(input('How many characters do you want in your password? '))
 password = ''
 
-while len(password) < passlength: password += str(map[random.randint(0, len(map) - 1)])
+while len(password) < passlength: password += str(symbols[random.randint(0, len(symbols) - 1)])
 
 print(password)
